e4a1f8821bed4f4bac0a0c44a4ed243c4d9c48dc
Moon Jun Hyeong <114069644+NoRuTnT@users.noreply.github.com>
2023-11-21 15:39:29 +0900

    전역변수

--- ssafit-vue-project/src/stores/auth.js
+++ ssafit-vue-project/src/stores/auth.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: sessionStorage.getItem('access-token') || null,
+    email: null,
   }),
   actions: {
     setToken(newToken) {
@@ -12,6 +13,18 @@ export const useAuthStore = defineStore('auth', {
     clearToken() {
       this.token = null;
       sessionStorage.removeItem('access-token');
-    }
+    },
+    updateEmailFromToken() {
+        if (this.token) {
+          const parts = this.token.split('.');
+          if (parts.length === 3) {
+            const payload = parts[1];
+            const decodedPayload = b64_to_utf8(payload);
+            this.email = JSON.parse(decodedPayload).email || null;
+          }
+        } else {
+          this.email = null;
+        }
+      }
   }
 });
